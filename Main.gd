d52573441bfeace2e5a8beeef2edef0f53a204c3
Joris Valette <joris.valette@algolia.com>
2021-04-25 18:39:29 +0200

    Add eye compass

--- Main.gd
+++ Main.gd
@@ -6,10 +6,13 @@ var current_face = 0
 
 func _input(e):
 	if e.is_action_pressed("ui_look_left"):
-		current_face = (current_face + 1) % 4
-		$ViewportContainer/Viewport/Tower.position.x = \
-			- current_face * Constants.COLS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.x
+		look_elsewhere(1)
 	if e.is_action_pressed("ui_look_right"):
-		current_face = (current_face + 3) % 4
-		$ViewportContainer/Viewport/Tower.position.x = \
-			- current_face * Constants.COLS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.x
+		look_elsewhere(-1)
+
+
+func look_elsewhere(direction):
+	current_face = (current_face + direction + 4) % 4
+	$ViewportContainer/Viewport/Tower.position.x = \
+		- current_face * Constants.COLS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.x
+	$HUD/Eye.rotate(direction * PI / 2)
