00d18f6d6c71dc14405a5d10845bd54fbca621eb
Joris Valette <joris.valette@algolia.com>
2021-04-26 12:23:43 +0200

    Add level numbers

--- Main.gd
+++ Main.gd
@@ -10,29 +10,34 @@ func _ready():
 	$ViewportContainer.margin_top = 100
 	$ViewportContainer/Viewport.size = Vector2(320, 640)
 	$ViewportContainer/Viewport/Tower.scale = Vector2(2, 2)
+	$HUD/LevelLabels/TopLevel.text = str(level_window)
+	$HUD/LevelLabels/BottomLevel.text = str(level_window + 1)
 
 
 func _input(e):
-	var tower = $ViewportContainer/Viewport/Tower
 	if e.is_action_pressed("ui_up"):
-		tower.current_level = max(0, tower.current_level - 1)
-		if tower.current_level < level_window:
-			level_window -= 1
-			tower.position.y = - level_window * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
-		$HUD/HighlightedLevel.rect_position.y = $ViewportContainer.margin_top + (tower.current_level - level_window) * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
+		look_up_down(-1)
 	if e.is_action_pressed("ui_down"):
-		tower.current_level = min(tower.nb_levels() - 1, tower.current_level + 1)
-		if tower.current_level > level_window + 1:
-			level_window += 1
-			tower.position.y = - level_window * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
-		$HUD/HighlightedLevel.rect_position.y = $ViewportContainer.margin_top + (tower.current_level - level_window) * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
+		look_up_down(1)
 	if e.is_action_pressed("ui_look_left"):
-		look_elsewhere(1)
+		look_left_right(1)
 	if e.is_action_pressed("ui_look_right"):
-		look_elsewhere(-1)
+		look_left_right(-1)
 
 
-func look_elsewhere(direction):
+func look_up_down(direction):
+	var tower = $ViewportContainer/Viewport/Tower
+	tower.current_level += direction
+	tower.current_level = clamp(tower.current_level, 0, tower.nb_levels() - 1)
+	if (direction == -1 and tower.current_level < level_window)	or (direction ==  1 and tower.current_level > level_window + 1):
+		level_window += direction
+		tower.position.y = - level_window * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
+		$HUD/LevelLabels/TopLevel.text = str(level_window)
+		$HUD/LevelLabels/BottomLevel.text = str(level_window + 1)
+	$HUD/HighlightedLevel.rect_position.y = $ViewportContainer.margin_top + (tower.current_level - level_window) * Constants.ROWS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.y
+
+
+func look_left_right(direction):
 	current_face = (current_face + direction + 4) % 4
 	$ViewportContainer/Viewport/Tower.position.x = \
 		- current_face * Constants.COLS * Constants.CELL_SIZE * $ViewportContainer/Viewport/Tower.scale.x
